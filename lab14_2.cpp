60f100addb68adcf424dbc6b90c076c1b9ec113d
monophoto <jetsadawut1013@gmail.com>
2023-01-13 17:18:14 +0700

    lab14complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,27 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool X[][M]){
+    cout << "------------------------------------------------------------------------" << endl;
+    for(int i=0; i<N; i++){
+        cout << "|";
+        for(int j=0; j<M;j++ ){
+            if(X[i][j] == 1)
+            cout<<"*";
+            else cout << " ";
+        }
+        cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------" << endl;
+}
+
+void updateImage(bool X[][M],int s,int x,int y){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M;j++ ){
+            if(pow((pow(i-x,2)+pow(j-y,2)),0.5) <= s-1)
+            X[i][j] = 1;
+        }
+        
+    }
+}
